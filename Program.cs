4ec4dcfbb7cd5b6c5367383e9abedfecf8ab2872
alex <alexandr.paramon56@gmail.com>
2025-06-17 20:09:18 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asynchronous_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
