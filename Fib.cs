eceb3b9ca9b0a2b67b06ca9d56092165d8d3d349
alex <alexandr.paramon56@gmail.com>
2025-06-17 23:11:05 +0500

    feat(core): Developed main functionality

---
+++ Fib.cs
@@ -0,0 +1,46 @@
+﻿namespace Asynchronous_methods;
+
+internal class Fib
+{
+    private int number;
+
+    public int Number { get; }
+
+    public decimal Result { get; private set; }
+
+    public Fib(int number)
+    {
+        this.number = number;
+        Calculate();
+    }
+
+    private void Calculate()
+    {
+        if (number <= 1)
+        {
+            Result = number;
+            return;
+        }
+
+        (decimal previous, decimal current) = (0, 1);
+        for (int i = 2; i < number; i++)
+        {
+            Result = previous + current;
+            previous = current;
+            current = Result;
+        }
+
+        //long odd = 1;
+        //long even = 1;
+        //for (int i = 1; i < 99999999 ; i++)
+        //{
+        //    if ((i % 2) == 0)
+        //    {
+        //        odd = even * i; 
+        //    } else
+        //    {
+        //        even = odd / i;
+        //    }
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Asynchronous_methods
+﻿using System.Diagnostics;
+
+namespace Asynchronous_methods;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        (int num1, int num2, int num3) = (75, 100, 130);
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        Console.WriteLine(new Fib(num1).Result);
+        Console.WriteLine(new Fib(num2).Result);
+        Console.WriteLine(new Fib(num3).Result);
+
+        sw.Stop();
+        Console.WriteLine($"Elapsed time: {sw.Elapsed}\n");
+
+        sw.Restart();
+
+        var task1 = FibAsync(num1);
+        var task2 = FibAsync(num2);
+        var task3 = FibAsync(num3);
+
+        var results = await Task.WhenAll(task1, task2, task3);
+
+        foreach (var result in results)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(result);
         }
+
+        sw.Stop();
+        Console.WriteLine($"Elapsed time: {sw.Elapsed}\n");
+
+        sw.Restart();
+
+        await Parallel.ForEachAsync<int>(new int[] { num1, num2, num3 }, async (int num, CancellationToken cancellation = default) => Console.WriteLine(await FibAsync(num)));
+
+        sw.Stop();
+        Console.WriteLine($"Elapsed time: {sw.Elapsed}\n");
+
+        Console.ReadLine();
+    }
+
+    static Task<decimal> FibAsync(int number)
+    {
+        return Task.Run(() => new Fib(number).Result);
     }
 }
